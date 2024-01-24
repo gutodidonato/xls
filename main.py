883f7c40c6bdad0b16801fd89fc06f7d7d86637f
gutodidonato <rm99433@fiap.com.br>
2024-01-24 17:13:19 -0300

    controle de loc, index, enumerate, info

--- main.py
+++ main.py
@@ -10,7 +10,24 @@ print(df_cadastro.info())
 tempo_salario = df_salario.merge(df_cadastro)
 servico_geral = tempo_salario.merge(df_servico)
 print(servico_geral)
+print("\n")
 
-for n in servico_geral['imposto']:
-    print(servico_geral["salario"])
-    
+for indice, valor_imposto in enumerate(servico_geral['imposto']):
+    if valor_imposto > 100:
+        valor_salario = servico_geral.loc[indice, 'salario']
+        valor_nome = servico_geral.loc[indice, 'Nome']
+        valor_salario = valor_salario - valor_imposto
+        print(f"{valor_nome} foi descontado/a {valor_imposto}, totalizando salario de: {valor_salario}")
+print("\n"*2)
+
+servico_geral = servico_geral.set_index('Nome')
+print(servico_geral.loc['John'])
+print("\n"*2)
+
+for index, codigo in enumerate(servico_geral['Codigo']):
+    if codigo == "BAA":
+        valor_nome = servico_geral.index[index]
+        valor_servico = input(f"Qual serviço deseja encaminhar: {valor_nome}? ")
+        servico_geral.loc[valor_nome, 'Serviço'] = valor_servico
+print("\n"*2)
+print(servico_geral)
